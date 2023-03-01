e3693a694e6141c06b800939ea1aff7be9251630
Andrew Loza <andrejloza81@gmail.com>
2023-03-01 19:12:35 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+//let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//let b = a[1] + a[4] + a[7] + a[9];
+
+//console.log(b);
+
+//let a = [1, 2, 3, 4, 5];
+
+//a[0] = 10;
+
+//a[1] = 20;
+
+//a[2] = 30;
+
+//a[3] = 40;
+
+//a[4] = 50;
+
+//console.log(a);
+
+//let a = [10, 20];
+
+//if (a[0] > a[1]) {
+// console.log(10);
+//} else {
+//console.log(20);
+//}
+
+//let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//let i = 0;
+
+//for (i = 0; i < a.length; i++) {
+//console.log(a[i]);
+//}
+//while (i <= 5) {
+//console.log(a[i]);
+//}
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; a[0] <= 5; ) {
+  console.log(a[0]++);
+}
+
+let c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let b = 0; b <= c.length; b++) {
+  if (b > 5 && b < 8) {
+    console.log(b);
+  }
+}
