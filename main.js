0fd58e820c802e1030d1e222582bfbd20f6792a2
Andrew Loza <andrejloza81@gmail.com>
2023-03-01 19:24:00 +0200

    second commit

--- main.js
+++ main.js
@@ -39,14 +39,18 @@
 
 let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (let i = 0; a[0] <= 5; ) {
-  console.log(a[0]++);
+for (let i = 0; i < a.length; i++) {
+  if (a[i] <= 5) {
+    console.log(a[i]);
+  }
 }
 
-let c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("---");
+
+let c = [1, 2, 14, 4, 5, 6, 7, 8, 9, 10];
 
 for (let b = 0; b <= c.length; b++) {
-  if (b > 5 && b < 8) {
-    console.log(b);
+  if (c[b] > 5 && c[b] < 8) {
+    console.log(c[b]);
   }
 }
